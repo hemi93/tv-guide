8ca383b1dc9743b88e49ea65083fe462350aeb28
Maciej Wądołkowski <maciej.wadolkowski@stermedia.pl>
2016-10-01 20:03:26 +0200

    Add test for creating schedule.

--- TVGuide/spec/requests/api/v1/schedules_controller_spec.rb
+++ TVGuide/spec/requests/api/v1/schedules_controller_spec.rb
@@ -3,13 +3,31 @@ describe Api::V1::SchedulesController do
     it 'returns HTTP 200 code' do
       get api_v1_schedules_path, as: :json
 
-      expect(response.status).to eq 200
+      expect(response).to have_http_status(200)
     end
 
     it 'returns Schedules matching JSON Schema' do
       get api_v1_schedules_path, as: :json
 
       expect(response).to match_response_schema('schedules')
+    end
+  end
+
+  describe 'POST /api/v1/schedules' do
+    let(:show) { create(:show) }
+    let(:airing_date) { Faker::Date.between(Date.today, 5.weeks.from_now) }
+    let(:schedule_params) { { schedule: { date: airing_date, show_id: show.id } } }
+
+    it 'returns HTTP 200 code if creating schedule was successful' do
+      post api_v1_schedules_path, params: schedule_params
+
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns JSON matching schedule schema if creating schedule was successful' do
+      post api_v1_schedules_path, params: schedule_params
+
+      expect(response).to match_response_schema('schedule')
     end
   end
 end
