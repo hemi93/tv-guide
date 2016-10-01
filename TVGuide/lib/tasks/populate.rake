84b40ead132c258714259f00320b440267da1899
Maciej Wądołkowski <maciej.wadolkowski@stermedia.pl>
2016-10-01 16:10:44 +0200

    Add factory for shows with schedules. Use in populating rake task.

--- TVGuide/lib/tasks/populate.rake
+++ TVGuide/lib/tasks/populate.rake
@@ -5,6 +5,6 @@ namespace :populate do
     desc 'Create sample data'
 
     puts 'Populating database with sample data...'
-    FactoryGirl.create_list(:channel, 5, :with_shows)
+    FactoryGirl.create_list(:channel, 5, :with_shows_and_schedules)
   end
 end
--- TVGuide/spec/factories/channels.rb
+++ TVGuide/spec/factories/channels.rb
@@ -12,5 +12,15 @@ FactoryGirl.define do
         create_list(:show, evaluator.shows_amount, channel: channel)
       end
     end
+
+    trait :with_shows_and_schedules do
+      transient do
+        shows_amount 10
+      end
+
+      after(:create) do |channel, evaluator|
+        create_list(:show, evaluator.shows_amount, :with_schedules, channel: channel)
+      end
+    end
   end
 end
