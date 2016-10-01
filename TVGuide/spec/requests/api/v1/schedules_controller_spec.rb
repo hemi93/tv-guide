fad6013f9bac21b011485eb9ce4c7f0a6703e3c5
Maciej Wądołkowski <maciej.wadolkowski@stermedia.pl>
2016-10-01 20:33:24 +0200

    Add test for schedule endpoints.

--- TVGuide/spec/requests/api/v1/schedules_controller_spec.rb
+++ TVGuide/spec/requests/api/v1/schedules_controller_spec.rb
@@ -17,17 +17,30 @@ describe Api::V1::SchedulesController do
     let(:show) { create(:show) }
     let(:airing_date) { Faker::Date.between(Date.today, 5.weeks.from_now) }
     let(:schedule_params) { { schedule: { date: airing_date, show_id: show.id } } }
+    let(:invalid_schedule_params) { { schedule: { date: nil, show_id: nil } } }
 
-    it 'returns HTTP 200 code if creating schedule was successful' do
+    it 'on success returns HTTP 200 code' do
       post api_v1_schedules_path, params: schedule_params
 
       expect(response).to have_http_status(200)
     end
 
-    it 'returns JSON matching schedule schema if creating schedule was successful' do
+    it 'on success returns JSON matching schedule schema' do
       post api_v1_schedules_path, params: schedule_params
 
       expect(response).to match_response_schema('schedule')
+    end
+
+    it 'on fail returns schedule errors JSON' do
+      post api_v1_schedules_path, params: invalid_schedule_params
+
+      expect(response).to match_response_schema('schedule_errors')
+    end
+
+    it 'on fail returns error code' do
+      post api_v1_schedules_path, params: invalid_schedule_params
+
+      expect(response).to have_http_status(400)
     end
   end
 end
