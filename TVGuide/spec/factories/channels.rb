63d34897e20ada4fd880a3779060e410f921b0ae
Maciej Wądołkowski <maciej.wadolkowski@stermedia.pl>
2016-10-01 15:22:01 +0200

    Add more complicated fake names to factories in order to avoid uniqueness validation issues.

--- TVGuide/spec/factories/channels.rb
+++ TVGuide/spec/factories/channels.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :channel do
-    name { Faker::Commerce.product_name }
+    name { "#{Faker::Commerce.product_name} #{Faker::Company.buzzword}" }
     category
   end
 end
