52c6e6e41cb4aa2a955cb96508ab536f01f933b3
Maciej Wądołkowski <maciej.wadolkowski@stermedia.pl>
2016-10-01 15:59:48 +0200

    Add basic rake task for populating database with sample data.

--- TVGuide/run_dev.sh
+++ TVGuide/run_dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -f tmp/pids/server.pid && rails db:migrate && rails db:setup && rails s -p 3000 -b '0.0.0.0'
+rm -f tmp/pids/server.pid && rails db:migrate && rails db:setup && rails populate:random_data && rails s -p 3000 -b '0.0.0.0'
--- TVGuide/spec/factories/channels.rb
+++ TVGuide/spec/factories/channels.rb
@@ -2,5 +2,15 @@ FactoryGirl.define do
   factory :channel do
     name { "#{Faker::Commerce.product_name} #{Faker::Company.buzzword}" }
     category
+
+    trait :with_shows do
+      transient do
+        shows_amount 10
+      end
+
+      after(:create) do |channel, evaluator|
+        create_list(:show, evaluator.shows_amount, channel: channel)
+      end
+    end
   end
 end
