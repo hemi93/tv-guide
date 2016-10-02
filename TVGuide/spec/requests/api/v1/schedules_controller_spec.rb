709bb7a359ce88e7bc2d0d27d4ff98bab108742c
Maciej Wądołkowski <maciej.wadolkowski@stermedia.pl>
2016-10-02 10:16:16 +0200

    Improve SchedulesController test coverage.

--- TVGuide/spec/requests/api/v1/schedules_controller_spec.rb
+++ TVGuide/spec/requests/api/v1/schedules_controller_spec.rb
@@ -1,6 +1,8 @@
 describe Api::V1::SchedulesController do
   let(:schedule) { create(:schedule) }
   let(:invalid_schedule_id) { 0 }
+  let(:invalid_schedule_params) { { schedule: { date: nil, show_id: nil } } }
+  let(:random_date) { Faker::Date.between(Date.today, 5.weeks.from_now) }
 
   describe 'GET /api/v1/schedules' do
     it 'returns HTTP 200 code' do
@@ -18,9 +20,8 @@ describe Api::V1::SchedulesController do
 
   describe 'POST /api/v1/schedules' do
     let(:show) { create(:show) }
-    let(:random_airing_date) { Faker::Date.between(Date.today, 5.weeks.from_now) }
-    let(:schedule_params) { { schedule: { date: random_airing_date, show_id: show.id } } }
-    let(:invalid_schedule_params) { { schedule: { date: nil, show_id: nil } } }
+
+    let(:schedule_params) { { schedule: { date: random_date, show_id: show.id } } }
 
     it 'on success returns HTTP 201 code' do
       post api_v1_schedules_path, params: schedule_params
@@ -48,22 +49,26 @@ describe Api::V1::SchedulesController do
   end
 
   describe 'PUT /api/v1/schedules/:id' do
-    let(:random_airing_date) { Faker::Date.between(Date.today, 5.weeks.from_now) }
-
     it 'on success returns HTTP 200 code' do
-      put api_v1_schedule_path(schedule), params: { schedule: { date: random_airing_date } }
+      put api_v1_schedule_path(schedule), params: { schedule: { date: random_date } }
 
       expect(response).to have_http_status(200)
     end
 
     it 'on success returns JSON matching schedule schema' do
-      put api_v1_schedule_path(schedule), params: { schedule: { date: random_airing_date } }
+      put api_v1_schedule_path(schedule), params: { schedule: { date: random_date } }
 
       expect(response).to match_response_schema('schedule')
     end
 
+    it 'on error returns JSON matching error schedule schema' do
+      put api_v1_schedule_path(schedule), params: invalid_schedule_params
+
+      expect(response).to match_response_schema('schedule_errors')
+    end
+
     it 'returns 404 status code if schedule was not found' do
-      put api_v1_schedule_path(id: invalid_schedule_id), params: { schedule: { date: random_airing_date } }
+      put api_v1_schedule_path(id: invalid_schedule_id), params: { schedule: { date: random_date } }
 
       expect(response).to have_http_status(404)
     end
