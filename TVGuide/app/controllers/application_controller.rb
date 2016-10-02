9467fcd0a19c299eb66f236d5de1ed0dc18ca8bf
Maciej Wądołkowski <maciej.wadolkowski@stermedia.pl>
2016-10-02 10:13:27 +0200

    Add tests for errors controller.

--- TVGuide/app/controllers/application_controller.rb
+++ TVGuide/app/controllers/application_controller.rb
@@ -1,14 +1,9 @@
 # Base class for non-api controllers in application
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  rescue_from ActionController::InvalidAuthenticityToken, with: :handle_invalid_authenticity_token
   rescue_from ActionController::RoutingError, with: :handle_routing_error
 
   private
-
-  def handle_invalid_authenticity_token
-    render json: { errors: [message: 'InvalidAuthenticityToken'] }, status: :unauthorized
-  end
 
   def handle_routing_error
     render json: { errors: [message: 'Path Not Found'] }, status: :not_found
